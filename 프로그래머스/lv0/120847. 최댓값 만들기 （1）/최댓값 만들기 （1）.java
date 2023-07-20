20c335039c5b335a52192ebff5f8efeb3b62365c
chanyong <128908315+yangchanyong@users.noreply.github.com>
2023-07-20 17:57:31 +0900

    주석 추가

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -4,9 +4,13 @@ import java.util.Comparator;
 
 class Solution {
     public int solution(int[] numbers) {
+        // Comparetor를 사용하기 위해 Integer배열 생성
         Integer[] nums = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
+        // 내림차순 정렬
         Arrays.sort(nums, Comparator.reverseOrder());
         int answer = nums[0] * nums[1];
         return answer;
     }
+
+    // 사실 오름차순 후 계산했어도 되는데 내림차순을 써보고 싶었음.... ㅎㅎ
 }
