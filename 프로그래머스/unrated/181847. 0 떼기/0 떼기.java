7cc67f9a0a90d5096cdc20d4003bddd3d3e30c1d
chanyong <128908315+yangchanyong@users.noreply.github.com>
2023-08-18 19:39:37 +0900

    [unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int result = Integer.parseInt(n_str);
+        answer = String.valueOf(result);
+        return answer;
+    }
+}
